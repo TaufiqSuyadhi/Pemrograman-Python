90e4b68d5d54252976cbf57aaf9c66375a33509b
Taufiq DS Suyadhi <robotics.university@gmail.com>
2022-06-11 18:01:25 +0700

    Update InputOutput.py

--- InputOutput/InputOutput.py
+++ InputOutput/InputOutput.py
@@ -1,10 +1,12 @@
 # Fungsi INPUT
 
+# Ketik nama sebagai input
 '''
 nama = input("Masukkan nama Anda: ")
 print(nama)
 '''
 
+# Jumlahkan 2 buah input berupa string
 '''
 dataIn1 = input("String1: ")
 dataIn2 = input("String2: ")
@@ -12,14 +14,16 @@ hasil = dataIn1 + dataIn2
 print(hasil)
 '''
 
+# input yang diketik berupa string, bukan int atau float, maka akan error
 '''
-# input = string bukan int atau float, shg error
 dataIn1 = input("Angka1: ")
 dataIn2 = input("Angka2: ")
 hasil = dataIn1 * dataIn2
 print(hasil)
 '''
 
+# Input yang diketik berupa string, bukan int atau float
+# Supaya tidak error, konversi dulu string menjadi int atau float
 """
 dataIn1 = input("Angka1: ")
 dataIn2 = input("Angka2: ")
@@ -27,11 +31,9 @@ hasil = int(dataIn1) + float(dataIn2)
 print("Hasil penjumlahan: ", hasil)
 """
 
-
-
 # Fungsi OUTPUT
-print("Hallo, Sahabat Robonesia")
+print("Hallo, Sahabat Robonesia")       # Menampilkan string (Kata/kalimat)
 print('Apa kabar teman-teman?')
 a = 7; b = 8
 c = a * b
-print("Hasil perkalian adalah: ", c)
+print("Hasil perkalian adalah: ", c)    # Menampilkan hasil operasi aritmatika
