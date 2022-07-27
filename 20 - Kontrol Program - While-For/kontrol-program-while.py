9ac4587f3fd876f6b4cfdb4748b13d54eec13e88
Taufiq DS Suyadhi <robotics.university@gmail.com>
2022-07-27 17:53:22 +0700

    Create kontrol-program-while.py

---
+++ 20 - Kontrol Program - While-For/kontrol-program-while.py
@@ -0,0 +1,18 @@
+# Kontrol Program - While
+
+'''
+# Perulangan While - Berbatas
+kalimat=input("Masukkan data: ")
+print("\nTampilkan data 5 kali \n")
+a = 1
+while(a<=5):                # Pembatas jumlah perulangan
+    print(a,".",kalimat)
+    a=a+1
+'''
+
+'''
+# Perulangan While - Tak Berbatas
+while True:                 # Jumlah perulangan tidak ada batas
+    print("Selamat Belajar Python")
+    print("di Robonesia")
+'''
